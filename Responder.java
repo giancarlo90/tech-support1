f513fe6e14382fd23289e747f58f4ad6134b21d8
Gian Carlo <giancarlo14.11@gmail.com>
2018-02-09 01:44:44 +0100

    Devuelve diferentes respuestas aleatorias a la hora de contestar

--- Responder.java
+++ Responder.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Random;
 /**
  * The responder class represents a response generator object.
  * It is used to generate an automatic response to an input string.
@@ -7,11 +9,16 @@
  */
 public class Responder
 {
+    private Random aleatorio;
+    private ArrayList <String> respuestas;
     /**
      * Construct a Responder - nothing to do
      */
     public Responder()
     {
+        aleatorio = new Random();
+        respuestas = new ArrayList<String>();
+        respuestasPredeterminadas();
     }
 
     /**
@@ -20,6 +27,16 @@ public class Responder
      */
     public String generateResponse()
     {
-        return "That sounds interesting. Tell me more...";
+        int numero = aleatorio.nextInt(respuestas.size());
+        return respuestas.get(numero);
+    }
+    
+    private void respuestasPredeterminadas()
+    {
+        respuestas.add("Eso suena interesante. Cuentame mas...");
+        respuestas.add("Hablame acerca de tu consulta...");
+        respuestas.add("Veamos como podria ayudarte...");
+        respuestas.add("Cuentame acerca del motivo de tu llamada...");
+        respuestas.add("Veamos como puedo ayudarte...");
     }
 }
